b6235ff206879bfef8483d750ecef9f0211fefd8
VLO <VLO@153.19.168.11>
2017-09-21 08:36:23 +0200

    Added red/blue buttons and spinner

--- RandomProgram/src/RandomProgram.java
+++ RandomProgram/src/RandomProgram.java
@@ -1,6 +1,16 @@
 import java.awt.EventQueue;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JToggleButton;
+import javax.swing.SpinnerNumberModel;
+
+import java.awt.Font;
+import java.awt.SystemColor;
+import javax.swing.JList;
+import javax.swing.JSpinner;
 
 public class RandomProgram {
 
@@ -34,8 +44,30 @@ public class RandomProgram {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 277, 382);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JToggleButton redButton = new JToggleButton("RED");
+		redButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+		redButton.setBackground(Color.RED);
+		redButton.setBounds(10, 293, 89, 40);
+		frame.getContentPane().add(redButton);
+		
+		JToggleButton blueButton = new JToggleButton("BLUE");
+		blueButton.setFont(new Font("Comic Sans MS", Font.BOLD, 15));
+		blueButton.setBackground(SystemColor.textHighlight);
+		blueButton.setBounds(165, 293, 89, 40);
+		frame.getContentPane().add(blueButton);
+		
+		ButtonGroup colorGroup = new ButtonGroup();
+		colorGroup.add(redButton);
+		colorGroup.add(blueButton);
+		
+		JSpinner numberSpinner = new JSpinner();
+		numberSpinner.setBounds(109, 294, 46, 42);
+		numberSpinner.setModel(new SpinnerNumberModel(0, 0, 36, 1));
+		JFormattedTextField formattedField=((JSpinner.DefaultEditor)numberSpinner.getEditor()).getTextField(); formattedField.setEditable(false); 
+		frame.getContentPane().add(numberSpinner);
 	}
-
 }
